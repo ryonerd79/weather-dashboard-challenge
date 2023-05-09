8b78cdd197adcdcbe1a3f7deefdca005dc5f0425
Muramori71 <dannyroh2500@gmail.com>
2023-05-09 09:07:18 -0700

    updated index and script files

--- Main/assets/script.js
+++ Main/assets/script.js
@@ -1,7 +1,9 @@
 var WeatherKy = "81579ebbf941cb5a0a56ea078ca19e47"
 var searchButton = document.querySelector("#search-button");
 var weatherInput =  document.querySelector("#input");
-const cityWeather = document.querySelector('.city-weather')
+const cityWeather = document.querySelector('.city-weather');
+  var rightNow = dayjs().format('MMM DD, YYYY');
+  $('#currentDay').text(rightNow);
 
 searchButton.addEventListener("click", function() {
     var city = weatherInput.value
@@ -12,7 +14,7 @@ searchButton.addEventListener("click", function() {
     })
     .then(function (data) {
       var cityName = data.city.name
-      console.log(cityWeather)//Loop over the data to generate a table, each table row will have a link to the repo url
+      console.log(data);//Loop over the data to generate a table, each table row will have a link to the repo url
      cityWeather.innerHTML = `
      <h1>${cityName}</h1>
      ` 
