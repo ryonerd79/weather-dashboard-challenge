5dd67c605e84ce6a95505f6918945bac747a4919
Muramori71 <dannyroh2500@gmail.com>
2023-05-08 22:01:45 -0700

    updated index style and script files

--- Main/assets/script.js
+++ Main/assets/script.js
@@ -1,18 +1,20 @@
 var WeatherKy = "81579ebbf941cb5a0a56ea078ca19e47"
 var searchButton = document.querySelector("#search-button");
 var weatherInput =  document.querySelector("#input");
-
+const cityWeather = document.querySelector('.city-weather')
 
 searchButton.addEventListener("click", function() {
     var city = weatherInput.value
-    var url = "http://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=" + WeatherKy;
+    var url = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=" + WeatherKy;
     fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      
+      var cityName = data.city.name
+      console.log(cityWeather)//Loop over the data to generate a table, each table row will have a link to the repo url
+     cityWeather.innerHTML = `
+     <h1>${cityName}</h1>
+     ` 
     });
 });
