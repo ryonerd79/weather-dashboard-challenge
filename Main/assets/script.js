5a2e0cd4afa119a71cff58b5db47cfa20309b75d
Muramori71 <dannyroh2500@gmail.com>
2023-05-10 06:52:11 -0700

    updated index style and script files

--- Main/assets/script.js
+++ Main/assets/script.js
@@ -1,5 +1,6 @@
 var WeatherKy = "81579ebbf941cb5a0a56ea078ca19e47"
 var searchButton = document.querySelector("#search-button");
+var cityButton = document.querySelector("#city-button");
 var weatherInput =  document.querySelector("#input");
 const cityWeather = document.querySelector('.city-weather');
   var rightNow = dayjs().format('MMM DD, YYYY');
@@ -16,7 +17,6 @@ searchButton.addEventListener("click", function() {
     })
     .then(function (data) {
       var cityName = data.city.name;
-      console.log(data)
       var weatherIcon = data.list[0].weather[0].icon;
       var temp = data.list[0].main.temp;
       var wind = data.list[0].wind.speed;
@@ -30,4 +30,27 @@ searchButton.addEventListener("click", function() {
      <p>Humidity: ${humidity}</p>
      ` 
     });
+});
+
+  cityButton.addEventListener("click", function() {
+  var city = cityWeather.value
+  var url = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=" + WeatherKy;
+  fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var cityName = data.city.name;
+    var weatherIcon = data.list[8].weather[8].icon;
+    var temp = data.list[8].main.temp;
+    var wind = data.list[8].wind.speed;
+    var humidity = data.list[8].main.humidity;
+   cityWeather.innerHTML = `
+   <h1>${cityName} ${rightNow}</h1>
+   <img src="https://openweathermap.org/img/w/${weatherIcon}.png" height="40" width="40">
+   <p>Temp: ${temp}</p>
+   <p>Wind: ${wind}</p>
+   <p>Humidity: ${humidity}</p>
+   ` 
+  });
 });
